51de3657f73412954cb351b6c9526e79a99e0aa6
Ewcia31 <ewa.krzystolik@gmail.com>
2024-01-21 22:11:59 +0100

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,23 +1,5 @@
-const galleryContainer = document.querySelector(".gallery");
-
-// Create HTML markup for the gallery
-const galleryMarkup = images
-  .map(
-    (image) => `
-    <li class="gallery-item">
-      <img src="${image.url}" alt="${image.alt}">
-    </li>
-  `
-  )
-  .join("");
-
-// Insert the gallery markup into the DOM in one operation
-galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
-
 const decreaseBtn = document.querySelector(`button[data-action="decrement"]`);
-
 const increaseBtn = document.querySelector(`button[data-action="increment"]`);
-
 const counterSpan = document.querySelector(`#value`);
 
 let counter = 0;
